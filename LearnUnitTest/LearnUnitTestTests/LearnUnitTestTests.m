5f984acb09d24c61ae522c737f8152c1a86a6efc
stonewin540 <stonewin540@163.com>
2014-03-11 17:13:00 +0800

    create an object to be tested.

--- LearnUnitTest/LearnUnitTestTests/LearnUnitTestTests.m
+++ LearnUnitTest/LearnUnitTestTests/LearnUnitTestTests.m
@@ -7,8 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "LUTObject.h"
 
 @interface LearnUnitTestTests : XCTestCase
+
+@property (nonatomic, strong) NSMutableArray *LUTObjects;
 
 @end
 
@@ -18,17 +21,38 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.LUTObjects = [[NSMutableArray alloc] init];
+    NSUInteger count = arc4random() % 10;
+    for (int i = 0; i < count; i++)
+    {
+        LUTObject *object = [[LUTObject alloc] init];
+        XCTAssert(nil != object, @"Nil object generated!!");
+        [self.LUTObjects addObject:object];
+    }
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.LUTObjects = nil;
     [super tearDown];
 }
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+//    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testCount
+{
+    XCTAssert(0 != [self.LUTObjects count], @"We have nothing!");
+    
+    [self.LUTObjects enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        LUTObject *lut = obj;
+        XCTAssert(nil != lut.message, @"Woo, object not init");
+        NSLog(@"%@", lut.message);
+    }];
 }
 
 @end
