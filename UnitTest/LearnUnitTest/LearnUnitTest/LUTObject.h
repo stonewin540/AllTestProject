8ea003cf487d098ab6fdf0bf62e98ca6d143e922
stonewin540 <stonewin540@163.com>
2014-03-11 17:21:37 +0800

    move project path

---
+++ UnitTest/LearnUnitTest/LearnUnitTest/LUTObject.h
@@ -0,0 +1,20 @@
+//
+//  LUTObject.h
+//  LearnUnitTest
+//
+//  Created by stone win on 3/6/14.
+//  Copyright (c) 2014 stone win. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+    A test class for project
+ */
+@interface LUTObject : NSObject
+
+@property (nonatomic, copy) NSString *objectId;
+@property (nonatomic, strong) NSNumber *duration;
+@property (nonatomic, copy) NSString *message;
+
+@end
