059b2f837b9ff92f521fbeac57ab157832b4455c
stone win <stone@stones-iMac.local>
2014-04-04 21:33:42 +0800

    a project for automation testing

---
+++ AutomationTest/AutomationTest/ATAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ATAppDelegate.h
+//  AutomationTest
+//
+//  Created by stone win on 4/4/14.
+//  Copyright (c) 2014 stone win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutomationTest/AutomationTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutomationTest
+//
+//  Created by stone win on 4/4/14.
+//  Copyright (c) 2014 stone win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ATAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ATAppDelegate class]));
+    }
+}
