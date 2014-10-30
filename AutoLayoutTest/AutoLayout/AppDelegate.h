adca07661c2bb6132cecb271dd8f14581fdda19c
stonewin540 <stonewin540@163.com>
2014-10-30 17:09:21 +0800

    add new test for AutoLayout

---
+++ AutoLayoutTest/AutoLayout/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by cai xuejun on 12-9-24.
+//  Copyright (c) 2012年 caixuejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AutoLayoutTest/AutoLayout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoLayout
+//
+//  Created by cai xuejun on 12-9-24.
+//  Copyright (c) 2012年 caixuejun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
