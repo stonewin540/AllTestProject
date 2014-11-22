499b7401931825112780bd4ab98b20d42b09d451
stonewin540 <stonewin540@163.com>
2014-11-22 10:31:52 +0800

    add ViewController for AutomationTest

---
+++ AutomationTest/AutomationTest/ATViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATViewController.h
+//  AutomationTest
+//
+//  Created by stone win on 4/4/14.
+//  Copyright (c) 2014 stone win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATViewController : UIViewController
+
+@end
---
+++ AutomationTest/AutomationTest/ATViewController.m
@@ -0,0 +1,61 @@
+//
+//  ATViewController.m
+//  AutomationTest
+//
+//  Created by stone win on 4/4/14.
+//  Copyright (c) 2014 stone win. All rights reserved.
+//
+
+#import "ATViewController.h"
+
+@interface ATViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@end
+
+@implementation ATViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)loadView
+{
+    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [self.view addSubview:_tableView];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
